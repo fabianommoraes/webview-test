70d3eb9a11f837e7e494ef1554381364a5b705a1
fabianommoraes <fabiano.m.moraes@gmail.com>
2021-03-15 13:02:14 -0300

    sem lib react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import * as isWebview from "is-ua-webview";
-import GoogleLogin from "react-google-login";
+// import GoogleLogin from "react-google-login";
 
 function App() {
   const webview = isWebview(navigator.userAgent);
@@ -37,13 +37,15 @@ function App() {
         <br />
         <br />
 
-        <GoogleLogin
+        <div className="g-signin2" data-onsuccess="onSignIn"></div>
+
+        {/* <GoogleLogin
           clientId="361624584650-9nbrs6phdrfspfub8ho23gnlvkdrta9b.apps.googleusercontent.com"
           buttonText="Login"
           onSuccess={onSuccess}
           onFailure={onFailure}
           cookiePolicy={"single_host_origin"}
-        />
+        /> */}
       </header>
     </div>
   );
